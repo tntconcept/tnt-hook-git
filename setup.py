467d1c63a468fd44224c59bb7c944280a0ef3e52
Daniel Otero <dotero@autentia.com>
2020-06-12 11:33:27 +0200

    Fixed missing dependency

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['requests'],
+    install_requires=['requests', 'keyring'],
     scripts=["bin/TNTHook"]
 )
