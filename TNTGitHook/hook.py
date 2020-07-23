5a4d0b8dbe1b6ca50ca18fc78dac3653a660f94a
Daniel Otero <dotero@autentia.com>
2020-07-23 10:34:37 +0200

    Added 5 seconds timeout

--- TNTGitHook/hook.py
+++ TNTGitHook/hook.py
@@ -109,7 +109,8 @@ def create_activity(config: Config,
     now = now.strftime("%Y-%m-%d")
     response: Response = requests.get(config.baseURL + "activities/",
                                       params={"startDate": str(now), "endDate": str(now)},
-                                      headers=headers)
+                                      headers=headers,
+                                      timeout=5)
     activities_response: List[ActivitiesResponse]
     activities_response = json.loads(response.text, object_hook=lambda x: to_class(x, cls=ActivitiesResponse))
     activities = reduce(lambda r, a: r + a.activities, activities_response, [])
@@ -133,7 +134,8 @@ def create_activity(config: Config,
 
     response: Response = requests.post(config.baseURL + "activities?autotruncate",
                                        headers=headers,
-                                       json=data)
+                                       json=data,
+                                       timeout=5)
     if response.status_code == 200:
         print("Successfully created activity for " + project_name + " - " + role_name)
 
@@ -147,7 +149,7 @@ def generate_request_headers(config):
     payload = {"grant_type": "password",
                "username": username,
                "password": password}
-    token_response = requests.post(config.authURL, headers=headers, data=payload)
+    token_response = requests.post(config.authURL, headers=headers, data=payload, timeout=5)
     if token_response.status_code != 200:
         raise AuthError()
     access_token = token_response.json()["access_token"]
@@ -157,7 +159,7 @@ def generate_request_headers(config):
 
 def check_role_exists(config, headers, project, role_name):
     response: Response = requests.get(config.baseURL + "projects/" + str(project.id) + "/roles",
-                                      headers=headers)
+                                      headers=headers, timeout=5)
     response.encoding = 'utf-8'
     roles: List[Role] = json.loads(response.text, object_hook=lambda x: to_class(x, cls=Role))
     role = first(lambda r: r.name == role_name, roles)
@@ -168,7 +170,7 @@ def check_role_exists(config, headers, project, role_name):
 
 def check_project_exists(config, headers, organization, project_name):
     response: Response = requests.get(config.baseURL + "organizations/" + str(organization.id) + "/projects",
-                                      headers=headers)
+                                      headers=headers, timeout=5)
     response.encoding = 'utf-8'
     projects: List[Project] = json.loads(response.text, object_hook=lambda x: to_class(x, cls=Project))
     project = first(lambda p: p.name == project_name, projects)
@@ -178,7 +180,7 @@ def check_project_exists(config, headers, organization, project_name):
 
 
 def check_organization_exists(config, headers, organization_name):
-    response: Response = requests.get(config.baseURL + "organizations", headers=headers)
+    response: Response = requests.get(config.baseURL + "organizations", headers=headers, timeout=5)
     response.encoding = 'utf-8'
     organizations: List[Organization] = json.loads(response.text, object_hook=lambda x: to_class(x, cls=Organization))
     organization = first(lambda o: o.name == organization_name, organizations)
