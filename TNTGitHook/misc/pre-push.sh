9059fd879905a5b9113ead41b39ed4ccfe4cfca7
Yair Segura <ysegura@autentia.com>
2021-05-27 18:29:41 +0200

    [FIX]: incorrect script reference

--- TNTGitHook/misc/pre-push.sh
+++ TNTGitHook/misc/pre-push.sh
@@ -4,4 +4,4 @@ set -o pipefail
 read local_ref local_sha remote_ref remote_sha
 
 # Assumes tnt_git_hook.sh is on PATH
-tnt_git_hook.sh "$local_ref" "$local_sha" "$remote_ref" "$remote_sha"
+tnt_git_hook "$local_ref" "$local_sha" "$remote_ref" "$remote_sha"
