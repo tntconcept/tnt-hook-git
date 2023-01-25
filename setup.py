e6850afaf20a5bdd1a1602be16ca8841e40e4cd2
Sara Subijana Gracia <sara.subijana@gmail.com>
2023-01-25 15:42:24 +0100

    Sets release candidate to 0.9.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TNTGitHook",
-    version="0.13.0",
+    version="0.9.0",
     author="Autentia",
     author_email="info@autentia.com",
     description="Utility to auto impute activities in TNT",
